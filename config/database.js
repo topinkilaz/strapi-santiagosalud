58bac0b540f58f6ea4a177c3c2b326de5c275ad0
topinkilaz <64761294+topinkilaz@users.noreply.github.com>
2024-09-25 09:23:39 -0400

    Update12 database.js

--- config/database.js
+++ config/database.js
@@ -8,10 +8,11 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false) && {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
     pool: { min: 2, max: 10 },
     acquireConnectionTimeout: 60000,
   },
 });
+ 
