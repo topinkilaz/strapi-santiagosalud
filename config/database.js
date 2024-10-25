d8a9436a082febac893ea36eb955c0c9a014f0ed
topinkilaz <juan.p.t.1234@gmail.com>
2024-10-25 14:50:34 -0400

    UpdateDB

--- config/database.js
+++ config/database.js
@@ -2,27 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      connectionString: env('DATABASE_URL'),
       ssl: {
         require: true,
         rejectUnauthorized: false
       },
-      pool: {
-        min: 0,
-        max: 1, // Reducimos el número máximo de conexiones
-        acquireTimeoutMillis: 300000, // Aumentamos el timeout
-        createTimeoutMillis: 300000,
-        destroyTimeoutMillis: 50000,
-        idleTimeoutMillis: 300000,
-        reapIntervalMillis: 1000,
-        createRetryIntervalMillis: 2000,
-      }
     },
-    debug: false,
-    acquireConnectionTimeout: 300000,
   },
 });
