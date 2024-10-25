40ad8da20cbcfb8699ada09e780f91e711c48105
topinkilaz <juan.p.t.1234@gmail.com>
2024-10-25 15:48:35 -0400

    UpdateDBR

--- config/database.js
+++ config/database.js
@@ -1,3 +1,21 @@
+
+// local coneccion
+/* module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+  },
+}); */
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
